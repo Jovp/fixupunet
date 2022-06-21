2be0809747439bfc4d8f8ee1779003c61bfd12d9
Julien Philip <juphilip@adobe.com>
2022-06-21 08:32:54 +0000

    Format to publish as a package

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elpips",
+    version="0.0.1",
+    author="Julien Philip",
+    author_email="juphilip@adobe.com",
+    description="Pytorch implementation of a modern unet using fixup initialization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jovp/fixupunet",
+    project_urls={
+        "Bug Tracker": "https://github.com/Jovp/fixupunet/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=[
+        "torch",
+        "torchvision",
+        "numpy",
+    ],
+)
