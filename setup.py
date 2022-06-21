f388c94e626159f49c51d8fefad8604385285e8b
Julien Philip <juphilip@adobe.com>
2022-06-21 08:45:01 +0000

    Update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="elpips",
+    name="fixupunet",
     version="0.0.1",
     author="Julien Philip",
     author_email="juphilip@adobe.com",
